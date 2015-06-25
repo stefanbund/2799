839c38289557efdafc6f1843330c05beef9097cd
stefan bund <stefan.bund@icloud.com>
2015-06-24 19:29:20 -0700

    lll

---
+++ wk1-2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html><head><meta charset="utf-8">
+
+<title>let's do a for-loop</title> </head>
+
+<body>
+
+<?php
+
+    for($i = 1; i <= 90; $i++)
+    {
+        echo "$i<br>";
+        
+    }
+?>
+
+</body></html>
